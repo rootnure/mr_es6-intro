ca9e2e0df0229bcf0863c14578702e9138db33d9
rootnure <nur.diu.2791@gmail.com>
2026-02-01 21:46:34 +0600

    var, let, const and TDZ

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 - ```var``` --> Hoisting (Not Recommended)
 - ```let``` --> Used for changeable value assign
 - ```const``` --> Used for constant value assign
+- Temporal Deadzone --> Time between hoisting and initialization of ```let``` / ```const```
---
+++ let-const.js
@@ -0,0 +1,26 @@
+// es6
+// const --> Can't change value
+const country = 'Bangladesh';
+const countryName = country + " (BD)";
+console.log(countryName);
+
+// let --> Used for changeable values
+let age = 20;
+age = 50;
+console.log(age);
+
+// can't access before initialization
+// console.log(year); // Temporal Dead Zone (Time between hoisting and initialization of let/const)
+const year = 2026;
+
+const chargePercentage = 100;
+// chargePercentage = 99;
+console.log(chargePercentage);
+
+const friends = ['Rakib', 'Sagor', 'Redwan'];
+friends.push('Rifat');
+console.log(friends);
+
+const person = {name: 'Ashiq', age: 20};
+person.gender = 'male';
+console.log(person);
